d226eb4ea0a3caea510f70644881e4405acafe2c
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-02-26 19:25:47 +0000

    variable & method setup

---
+++ src/project01/LibraryCatalog.java
@@ -0,0 +1,39 @@
+package project01;
+
+public class LibraryCatalog {
+	
+	//variables
+	private ArrayList<Book> books;
+	
+	//constructor
+	
+	//getters - does this include getBook?
+	
+	//setters
+	
+	public void add(Book book)
+	{
+		
+	}
+	
+	public Book getBook(String title)
+	{
+		
+	}
+	
+	public boolean checkoutBook(String title)
+	{
+		
+	}
+	
+	public boolean returnBook(String title)
+	{
+		
+	}
+	
+	public void addBooksFromFile(String filename)
+	{
+		
+	}
+
+}
