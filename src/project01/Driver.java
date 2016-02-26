bd109d4f3a3fe51faf97985baf86debc835439b0
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-02-26 19:24:43 +0000

    variable & method setup

---
+++ src/project01/Driver.java
@@ -0,0 +1,16 @@
+package project01;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		LibraryCatalog book = new LibraryCatalog();
+		book.addBooksFromFile("booklist");
+		
+		UserInerface ui = new UserInterface();
+		ui.mainLoop(book);
+		
+
+	}
+
+}
