45a1e74785b07a4e87e7bf57245fe1ddc0f9a86c
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-02-28 06:42:10 +0000

    completed constructor

--- src/project01/Book.java
+++ src/project01/Book.java
@@ -2,8 +2,42 @@ package project01;
 
 public class Book {
 	
+	//variables
 	private String title, author, publisher;
 	private int year;
 	private boolean isCheckedOut;
+	
+	//constructor
+	public Book(String title, String author, int year, String publisher, boolean isCheckedOut)
+	{
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.publisher = publisher;
+		isCheckedOut =  false;
+	}
+	
+	//getters
+	
+	//setters
+	
+	//String toString
+	
+	public String toString()
+	{
+		
+	}
+	
+	//other methods
+	
+	public void checkoutBook()
+	{
+		
+	}
+	
+	public void returnBook()
+	{
+		
+	}
 
 }
