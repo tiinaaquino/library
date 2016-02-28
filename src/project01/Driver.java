fff33bab15a7102ce117b75fc40258e1f8c621d9
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-02-28 08:04:44 +0000

    shortened comments for testing book class

--- src/project01/Driver.java
+++ src/project01/Driver.java
@@ -6,49 +6,42 @@ public class Driver {
 		
 		// testing out Book class:
 		/*
-		 * the book is available
-		 * you can checkout the book
-		 * once checked out, availability should be false
-		 * isCheckedOut should become false
-		 
+		 * the book is available & can check it out
+		 * once checked out, availability & isCheckedOut should be false
 		Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, "Warner Books, Inc.", true);
 		book1.checkoutBook();
 		System.out.println(book1);
 		
-		 *  the book is already borrowed
-		 *  you cannot checkout the book
-		 *  availability should be false
-		 * isCheckedOut should equal false
+		 *  the book is already borrowed, cannot check it out
+		 *  availability/ isCheckedOut should be false
 		Book book2 = new Book("The Awakening", "Kate Chopin", 1899, "Dover Publications", false);
 		book2.checkoutBook();
 		System.out.println(book2);
 		
-		 *  the book is available
-		 *  you can return the book
-		 *  availability should be true
-		 * isCheckedOut should equal true
+		 *  the book is available, can return it
+		 *  availability/ isCheckedOut should be true
 		Book book3 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951, "Little, Brown and Company", false);
 		book3.returnBook();
 		System.out.println(book3);
 		
 		 *  the book is available
 		 *  you can checkout the book, availability = false
-		 *  you can return book, availability = true
-		 * isCheckedOut should equal true
+		 *  you can return book, availability/ isCheckedOut= true
 		Book book4 = new Book("Where the SideWalk Ends", "Shel Silverstein", 1974, "HarperCollins", true);
 		book4.checkoutBook();
 		book4.returnBook();
 		System.out.println(book4);
 		
-		 *  the book is available
-		 *  you cannot return the book because it was never borrowed
-		 *  availability should be true
-		 * isCheckedOut should equal true
+		 *  the book is available, cannot return the book because book never borrowed
+		 *  availability/ isCheckedOut should be true
 		Book book5 = new Book("Slaughterhouse-Five", "Kurt Vonnegut", 1969, "Dell Publishing", true);
 		book5.returnBook();
 		System.out.println(book5);
 		*/
 
+		//testing out LibraryCatalog class:
+		
+		
 		
 		/* final code =
 		LibraryCatalog book = new LibraryCatalog();
