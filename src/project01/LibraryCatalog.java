1dab35da9fb21c47775776cccd8296773775eac6
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-03-03 06:40:00 +0000

    fixed computer output to user

--- src/project01/LibraryCatalog.java
+++ src/project01/LibraryCatalog.java
@@ -113,7 +113,7 @@ public class LibraryCatalog {
 			{
 				if (bookName.getIsCheckedOut() == true)
 				{
-					System.out.println(title + "is successfully returned.");
+					System.out.println(title + " is successfully returned.");
 					bookName.returnBook();
 					return true;
 				}
