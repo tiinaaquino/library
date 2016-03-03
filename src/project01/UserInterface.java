06b6d430e77488ba967a2369c04adda6aae08e47
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-03-03 03:28:44 +0000

    comments = complete

--- src/project01/UserInterface.java
+++ src/project01/UserInterface.java
@@ -11,24 +11,28 @@ public class UserInterface {
 	
 	public void mainLoop(LibraryCatalog bc)
 	{
+		//first scanner to take in 1, 2, 3, or 4
+		//second scanner takes in user's commands/ actions for books
 		Scanner scan = new Scanner(System.in);
 		Scanner scanner = new Scanner(System.in);
 		
-		//2 while loops to except second input? or 2 instances of Scanner
+		//use while loop to repeatedly prompt the user until they exit
 		
 		while (true)
 		{
 			System.out.println("Enter 1 to check out a book, 2 to return a book, 3 to print the list of available books, or 4 to exit: ");
 			input = scan.nextLine();
 			
+			//checks out a book
 			if (input.equals("1"))
 			{
 				System.out.println("Checking out the book? Please enter the title: ");
 				input2 = scanner.nextLine();
-				//search for the book
+				
 				bc.checkoutBook(input2);
 			}
 			
+			//returns a book
 			if (input.equals("2"))
 			{
 				System.out.println("Returning the book? Please enter the title: ");
@@ -37,11 +41,13 @@ public class UserInterface {
 				bc.returnBook(input2);
 			}
 			
+			//prints list of available books
 			if (input.equals("3"))
 			{
-				//library.printAvailableBooks();
+				//bc.printAvailableBooks();
 			}
 			
+			//exit & breaks out of loop, terminating the program
 			if (input.equals("4"))
 			{
 				break;
